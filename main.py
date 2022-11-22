dad11a4c3933fe56d41c5c3115f32f16df9592cd
Алексей Крахотин <lasagnamaster@yandex.ru>
2022-11-22 10:40:22 +0300

    Basics

--- main.py
+++ main.py
@@ -2,14 +2,61 @@ import pygame, math
 
 pygame.init()
 
+WIDTH = 1080
+HEIGHT = 720
+
+sc = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Chess'N'Guns")
+
 FPS = 60
 clock = pygame.time.Clock()
 
-while 1:
+class Figure:
+	"""
+	initialize figure
+	color 0 - white
+	color 1 - black
+	"""
+	def __init__(x = 0, y = 0):
+		self.x = x
+		self.y = y
+		self.color = 0
+		self.type = 0
+
+class Pawn(Figure):
+	def __init__():
+		pass
+
+class Ladya(Figure):
+	def __init__():
+		pass
+
+class Slon(Figure):
+	def __init__():
+		pass
+
+class Horse(Figure):
+	def __init__():
+		pass
+
+class Queen(Figure):
+	def __init__():
+		pass
+
+class King(Figure):
+	def __init__():
+		pass
+
+figures_position = []
+desk = []
+finished = False
+
+while not finished:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			exit()
 
 	clock.tick(FPS)
 
+	#sc.blit(rendered_game, (0,0))
 	pygame.display.update()
---
+++ steps.py
@@ -0,0 +1,41 @@
+def check_step(figure, desk):
+    """
+    types:
+    type 0 - pawn
+    type 1 - lad'ya
+    type 2 - slon
+    type 3 - horse
+    type 4 - queen
+    type 5 - king
+    """
+    steps = [] 
+    if figure.color == 0: k = -1
+    else: k = 1
+
+    if figure.type == 0:
+        #not finished!!
+        if figure.moved == False:
+            if desk[self.x][self.y+k]==False:
+                steps.append([0,1])
+            if desk[self.x][self.y+2*k]==False:
+                steps.append([0,2])
+        else:
+            if desk[self.x][self.y+k]==False:
+                steps.append([0,1])
+
+    if figure.type == 1:
+        pass
+
+    if figure.type == 2:
+        pass
+
+    if figure.type == 3:
+        pass
+
+    if figure.type == 4:
+        pass
+
+    if figure.type == 5:
+        pass
+
+    return steps
